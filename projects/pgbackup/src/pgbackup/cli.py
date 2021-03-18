9eaa762ec76866a8fcfbb3ea5663c8c4a7042bf3
edinom <edino.moniz@sectigo.com>
2021-03-18 17:09:15 +0000

    Created pgbackup package and cli module

---
+++ projects/pgbackup/src/pgbackup/cli.py
@@ -0,0 +1,20 @@
+from argparse import Action, ArgumentParser
+
+class DriverAction(Action):
+    def __call__(self, parser, namespace, values, option_string=None):
+        driver, destination = values
+        namespace.driver = driver.lower()
+        namespace.destination = destination
+
+def create_parser():
+    parser = ArgumentParser(description="""
+    Backup Postgresql databases locally or to AWS S3.
+    """)
+
+    parser.add_argument("url", help="URL of the database to backup")
+    parser.add_argument("--driver",
+            help="how and wgere to store the backup",
+            nargs=2,
+            action=DriverAction,
+            required=True)
+    return parser
