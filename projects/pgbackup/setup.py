4e5db7f7fa46c4d558c21a2c062fcebda07bc787
edinom <edino.moniz@sectigo.com>
2021-03-18 17:08:05 +0000

    setup.py file

---
+++ projects/pgbackup/setup.py
@@ -0,0 +1,17 @@
+from setuptools import find_packages, setup
+
+with open("README.md", "r") as f:
+    description = f.read()
+
+setup(
+    name ="pgbackup",
+    version="0.1.0",
+    description="A utility to backup Postgresql databases",
+    url="https://github.com/edinomoniz/sixmonthpython",
+    packages=find_packages("src"),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Topic :: System :: Archiving :: Backup",
+        "Programming Language :: Python :: 3.9",
+    ],
+)
